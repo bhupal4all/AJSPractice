151af37ef8f9b0ac04928c103729b29127b9367c
Ranga Bhupal Reddy Gangavarapu <RGangavarapu@manh.com>
2016-01-31 13:53:58 +0530

    Routing | Route service

--- src/main/webapp/resources/js/practice/controller/routing-route.js
+++ src/main/webapp/resources/js/practice/controller/routing-route.js
@@ -3,29 +3,15 @@
 ///////////////////////////////////////////////////////////////////////////////	
 angular.module('mainModule', ['ngRoute'])
 	
-	.provider('routeManager', function($routeProvider){
-		var scope = null;
-		return{
-			$get: function(){
-				return{
-					setScope: function(outScope){
-						scope = outScope;
-					}
-				};
-			},
-			configRoutes: function(){
-				$routeProvider
-					.when('/viewone', {
-						templateUrl: '/angularjs/views/viewone' 
-					});
-			}
-		};
-	})
-
 	.config(['$routeProvider', function($routeProvider){
+		console.log('from config');
+
 		$routeProvider
 			.when('/default', {
 				templateUrl: '/angularjs/views/default.jsp'
+			})
+			.when('/viewone', {
+				templateUrl: '/angularjs/views/viewone.jsp'
 			})
 			.otherwise({ 
 				redirectTo: '/default' 
@@ -33,5 +19,5 @@ angular.module('mainModule', ['ngRoute'])
 	}])
 
 	.controller('mainController', ['$scope', function ($scope) {
-		console.log('Routing');
+		console.log('from controller');
 	}]);
